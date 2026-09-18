91299efb16733f9a116b0ac9c45f6fc66a99d817
fatonyist92-jpg <fatonyist.92@gmail.com>
2026-09-18 16:39:11 +0700

    Add app/src/main/java/com/fatoni/diza/voice/VoiceController.kt

---
+++ app/src/main/java/com/fatoni/diza/voice/VoiceController.kt
@@ -0,0 +1,7 @@
+package com.fatoni.diza.voice
+
+/**
+ * Reserved buat native audio controls berikutnya.
+ * V0.2.1 memakai WebRTC di avatar WebView sebagai jalur voice utama.
+ */
+class VoiceController
