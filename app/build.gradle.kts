97657115968938324724208ea665f0a9e876929d
fatonyist92-jpg <fatonyist.92@gmail.com>
2026-09-18 18:47:17 +0700

    Set Diza visual test v0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.fatoni.diza"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.3.0"
         buildConfigField(
             "String",
             "DIZA_REALTIME_BACKEND_URL",
